d2300bd6413043b41578f83a1100dcc3c26c5e7a
marcosfrank <marcosfrank6@gmail.com>
2014-11-04 09:58:04 -0300

    Sitio Fraviser

---
+++ js/imagen.js
@@ -0,0 +1,33 @@
+﻿/// <reference path="jquery-1.11.0.js" />
+$(document).ready(function () {
+    desplazaDerecha();
+});
+
+function desplazaDerecha() {
+    $("#sembradora").animate({ left: "+=450px" }, 5000, desapareceSembradora);
+    $("#tierraArada").animate({ width: "+=450px" }, 5000);
+}
+
+function desapareceSembradora() {
+    $("#sembradora").fadeOut("slow",apareceSembradora);
+    
+}
+
+function apareceSembradora() {
+    $("#sembradora").css("top", "+=150px");
+    $("#sembradora").fadeIn("slow",desplazaIzquierda);
+}
+
+function desplazaIzquierda() {
+    $("#sembradora").animate({ left: "-=450px" }, 5000, desapareceSembradora);
+    $("#tierraArada2").animate({ left: "-=450px", width: "+=450px" }, 5000);
+}
+
+
+function desplazar(ladoInicio, piso, accion) { 
+    
+}
+
+
+
+
