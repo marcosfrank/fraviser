b0860faa8688179cb35e241a98a4fcfe6aba0c32
marcosfrank <marcosfrank6@gmail.com>
2014-11-04 13:57:45 -0300

    Funcionalidad de sembrado completada

--- js/imagen.js
+++ js/imagen.js
@@ -1,33 +1,59 @@
 ﻿/// <reference path="jquery-1.11.0.js" />
+
 $(document).ready(function () {
-    desplazaDerecha();
+    function desplazar() {
+        claseTierra = "." + tierra.toString() + ".nivel" + piso.toString();
+        claseMaquina = "." + maquina.toString();
+        if (piso == 1 || piso == 3) {
+            $(claseMaquina).animate({ left: "+=480px" }, 5000, desapareceSembradora);
+            $(claseTierra).animate({ width: "+=450px" }, 5000);
+        } else {
+            $(claseMaquina).animate({ left: "-=480px" }, 5000, desapareceSembradora);
+            $(".tierra.nivel2").animate({ width: "-=450px" }, 5000);
+        }
+    }
+
+
+    function desapareceSembradora() {
+        $(claseMaquina).fadeOut("slow", apareceSembradora);
+    }
+
+    function apareceSembradora() {
+        piso++;
+        if (piso != 4) {
+            $(claseMaquina).css("top", "+=150px");
+            $(claseMaquina).fadeIn("slow", desplazar);
+        }
+    }
+
+    var piso = 1;
+    var tierra = "tierraSembrada";
+    var maquina = "sembradora";
+    var claseTierra = "";
+    var claseMaquina = "";
+
+    desplazar();
 });
 
-function desplazaDerecha() {
-    $("#sembradora").animate({ left: "+=450px" }, 5000, desapareceSembradora);
-    $("#tierraArada").animate({ width: "+=450px" }, 5000);
-}
+//function desplazaDerecha() {
+//    $("#sembradora").animate({ left: "+=450px" }, 5000, desapareceSembradora);
+//    $("#tierraSembrada").animate({ width: "+=450px" }, 5000);
+//}
 
-function desapareceSembradora() {
-    $("#sembradora").fadeOut("slow",apareceSembradora);
-    
-}
+//function desapareceSembradora() {
+//    $("#sembradora").fadeOut("slow",apareceSembradora);
+//    
+//}
 
-function apareceSembradora() {
-    $("#sembradora").css("top", "+=150px");
-    $("#sembradora").fadeIn("slow",desplazaIzquierda);
-}
+//function apareceSembradora() {
+//    $("#sembradora").css("top", "+=150px");
+//    $("#sembradora").fadeIn("slow",desplazaIzquierda);
+//}
 
-function desplazaIzquierda() {
-    $("#sembradora").animate({ left: "-=450px" }, 5000, desapareceSembradora);
-    $("#tierraArada2").animate({ left: "-=450px", width: "+=450px" }, 5000);
-}
-
-
-function desplazar(ladoInicio, piso, accion) { 
-    
-}
-
+//function desplazaIzquierda() {
+//    $("#sembradora").animate({ left: "-=450px" }, 5000, desapareceSembradora);
+//    $("#tierraArada2").animate({ left: "-=450px", width: "+=450px" }, 5000);
+//}
 
 
 
