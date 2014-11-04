5a132f8ec77ce6dc47cbc01d0a0a3761c97c6b38
marcosfrank <marcosfrank6@gmail.com>
2014-11-04 17:29:06 -0300

    Sembrado con curvas y constante de tiempo realizado

--- js/imagen.js
+++ js/imagen.js
@@ -1,38 +1,58 @@
 ﻿/// <reference path="jquery-1.11.0.js" />
 
 $(document).ready(function () {
-    function desplazar() {
-        claseTierra = "." + tierra.toString() + ".nivel" + piso.toString();
-        claseMaquina = "." + maquina.toString();
-        if (piso == 1 || piso == 3) {
-            $(claseMaquina).animate({ left: "+=480px" }, 5000, desapareceSembradora);
-            $(claseTierra).animate({ width: "+=450px" }, 5000);
-        } else {
-            $(claseMaquina).animate({ left: "-=480px" }, 5000, desapareceSembradora);
-            $(".tierra.nivel2").animate({ width: "-=450px" }, 5000);
-        }
-    }
-
-
-    function desapareceSembradora() {
-        $(claseMaquina).fadeOut("slow", apareceSembradora);
-    }
-
-    function apareceSembradora() {
-        piso++;
-        if (piso != 4) {
-            $(claseMaquina).css("top", "+=150px");
-            $(claseMaquina).fadeIn("slow", desplazar);
-        }
-    }
-
+    const tiempoAnimacion = 2000;
     var piso = 1;
+    var tierraFondo = "tierra"
     var tierra = "tierraSembrada";
     var maquina = "sembradora";
     var claseTierra = "";
     var claseMaquina = "";
-
     desplazar();
+
+
+
+    function desplazar() {
+        claseTierraFondo = "." + tierraFondo.toString() + ".nivel" + piso.toString();
+        claseTierra = "." + tierra.toString() + ".nivel" + piso.toString();
+        claseMaquina = "." + maquina.toString();
+        if (piso == 1 || piso == 3) {
+            $(claseMaquina).animate({ left: "+=480px" }, tiempoAnimacion, rotarMaquina);
+            $(claseTierra).animate({ width: "+=450px" }, tiempoAnimacion);
+        } else {
+            $(claseMaquina).animate({ left: "-=480px" }, tiempoAnimacion, rotarMaquina);
+            $(claseTierraFondo).animate({ width: "-=450px" }, tiempoAnimacion);
+        }
+    }
+
+     function rotarMaquina() {
+        var $elem = $(claseMaquina);
+            
+        if(piso<3){
+            var angulo;
+
+            if(piso==1 || piso == 3){
+                angulo = 180;
+            }else{
+                angulo = -180;
+                $elem.css("top","+=150px");
+            }
+            piso++;
+            $({ deg: 0 }).animate({ deg: angulo }, {
+                duration: tiempoAnimacion,
+                step: function (now) {
+                    $elem.css({
+                        transform: 'rotate(' + now + 'deg)',
+                    });
+                },
+               complete:desplazar
+            });
+        }else{
+            $elem.fadeOut(600);
+        }
+    }
+
+    
 });
 
 //function desplazaDerecha() {
